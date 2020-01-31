b3349af44eb7f820fa21b57677a380cf13613544
alitvinov <alexlitvinovpost@gmail.com>
2020-01-31 12:57:03 +0200

    feature1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,8 +3,9 @@ public class Test {
     static enum Test2 {A,B,C;}
 
     public static void main(String[] args) throws Exception {
-       Test2 a = null;//3
-       //2
+       Test2 a = null;//1
+        //feature
+        //master
        if (a == Test2.A) {
            System.out.println("11111");
        } else {
