b9f0594bf571a245575b4f6befd2ec3908d04cf0
alitvinov <alexlitvinovpost@gmail.com>
2020-01-31 12:50:15 +0200

    333333

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,8 @@ public class Test {
     static enum Test2 {A,B,C;}
 
     public static void main(String[] args) throws Exception {
-       Test2 a = null;//1
+       Test2 a = null;//3
+       //2
        if (a == Test2.A) {
            System.out.println("11111");
        } else {
